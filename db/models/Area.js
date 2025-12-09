04650930cd740c2f6a63f3d36d5c3fe9a7590102
Maksym Rutkovskyi <maxikrud0071@gmail.com>
2025-12-09 12:29:00 +0200

    add id key to the Area model

--- db/models/Area.js
+++ db/models/Area.js
@@ -2,6 +2,12 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../sequelize.js';
 
 const Area = sequelize.define('area', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
   name: {
     type: DataTypes.STRING,
     allowNull: false
