f5f19fdc35f8b8034cc20d49609f1b711bd81a1a
Maksym Rutkovskyi <maxikrud0071@gmail.com>
2025-12-09 22:17:23 +0200

    Delete routes/testimonialRouter.js

--- routes/testimonialRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAll } from "../controllers/TestimonialController.js";
-
-const testimonialRouter = Router();
-
-testimonialRouter.get("/", getAll);
-
-export default testimonialRouter;
