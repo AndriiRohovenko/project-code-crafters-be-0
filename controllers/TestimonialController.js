6c4fb809ade242d8f60ad5704b0d452966a94389
Maksym Rutkovskyi <maxikrud0071@gmail.com>
2025-12-10 21:23:07 +0200

    implement user name fetching on testimonials fetch

--- controllers/TestimonialController.js
+++ controllers/TestimonialController.js
@@ -3,12 +3,14 @@ import HttpError from "../helpers/HttpError.js";
 
 export const getAll = async (req, res, next) => {
   try {
-    // TODO: Add user name to response when user model is created
     const testimonials = await getTestimonials();
-    const result = testimonials.map(testimonial => ({
-      ...testimonial.toJSON(),
-      owner: "Hardcoded User"
-    }));
+    const result = testimonials.map(testimonial => {
+      const { owner, ownerId, ...rest } = testimonial.toJSON();
+      return {
+        ...rest,
+        ownerName: owner?.name || "Unknown"
+      };
+    });
     res.json(result);
   } catch (error) {
     next(HttpError(500));
--- services/TestimonialService.js
+++ services/TestimonialService.js
@@ -1,7 +1,20 @@
 import Testimonial from "../db/models/Testimonial.js";
+import User from "../db/models/User.js";
 
 async function getTestimonials() {
-  return await Testimonial.findAll();
+
+  return await Testimonial.findAll({
+    attributes: {
+      exclude: ["ownerId", "createdAt", "updatedAt"]
+    },
+    include: [
+      {
+        model: User,
+        as: "owner",
+        attributes: ["name"]
+      }
+    ],
+  });
 }
 
 export { getTestimonials };
