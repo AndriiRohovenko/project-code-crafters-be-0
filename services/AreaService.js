8fe0d637b376f6c6ff2376e6d803675bc7171cf5
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-12-22 15:35:48 +0200

    feat: add sorting by name to area, category, and ingredient retrieval functions

--- services/AreaService.js
+++ services/AreaService.js
@@ -1,7 +1,9 @@
 import Area from '../db/models/Area.js';
 
 async function getAreas() {
-  return await Area.findAll();
+  return await Area.findAll({
+    order: [['name', 'ASC']],
+  });
 }
 
 export { getAreas };
